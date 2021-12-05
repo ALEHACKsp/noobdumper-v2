fd1c111caaf14b040eceaf73f956bbcc6ce44a09
noobes <68382500+n00bes@users.noreply.github.com>
2021-12-05 13:01:20 +0200

    Update hooks.h

--- hooks/hooks.h
+++ hooks/hooks.h
@@ -2,6 +2,7 @@
 #include <Windows.h>
 #include <time.h>
 
+#include <tlhelp32.h>
 
 #include <MinHook.h>
 #pragma comment (lib, "minhook")
@@ -9,48 +10,79 @@
 #include <stdio.h>
 
 
-typedef BOOL( WINAPI* tWriteProcessMemory )
-( HANDLE  hProcess,
-  LPVOID  lpBaseAddress,
-  LPVOID  lpBuffer,
-  DWORD   nSize,
-  LPDWORD lpNumberOfBytesWritten );
+//Type definitions
+//////////////////
+typedef NTSTATUS( NTAPI* tNtWriteVirtualMemory )
+( 
+  HANDLE hProcess,
+  PVOID pBaseAddress,
+  PVOID pBuffer,
+  ULONG uNumberOfBytesToWrite,
+  PULONG puNumberOfBytesWritten
+);
 
-typedef LPVOID(
-    WINAPI*
-    tVirtualAllocEx )(
-    _In_ HANDLE hProcess,
-    _In_opt_ LPVOID lpAddress,
-    _In_ SIZE_T dwSize,
-    _In_ DWORD flAllocationType,
-    _In_ DWORD flProtect
-    );
+//typedef BOOL( WINAPI* tWriteProcessMemory )
+//( 
+//  HANDLE  hProcess,
+//  LPVOID  lpBaseAddress,
+//  LPVOID  lpBuffer,
+//  DWORD   nSize,
+//  LPDWORD lpNumberOfBytesWritten 
+//);
 
-
-
-BOOL
-WINAPI
-hkWriteProcessMemory( HANDLE, LPVOID, LPVOID, DWORD, LPDWORD );
-
-LPVOID
-WINAPI
-hkVirtualAllocEx(
-    _In_ HANDLE hProcess,
-    _In_opt_ LPVOID lpAddress,
-    _In_ SIZE_T dwSize,
-    _In_ DWORD flAllocationType,
-    _In_ DWORD flProtect
+typedef LPVOID( WINAPI* tVirtualAllocEx )
+( 
+  _In_ HANDLE hProcess,
+  _In_opt_ LPVOID lpAddress,
+  _In_ SIZE_T dwSize,
+  _In_ DWORD flAllocationType,
+  _In_ DWORD flProtect
 );
 
 
-extern tWriteProcessMemory oWriteProcessMemory;
+
+//Functions
+///////////
+
+NTSTATUS
+NTAPI
+hkNtWriteVirtualMemory
+(
+	HANDLE hProcess,
+	PVOID pBaseAddress,
+	PVOID pBuffer,
+	ULONG uNumberOfBytesToWrite,
+	PULONG puNumberOfBytesWritten
+);
+
+//BOOL
+//WINAPI
+//hkWriteProcessMemory
+//( 
+//  HANDLE,
+//  LPVOID,
+//  LPVOID,
+//  DWORD,
+//  LPDWORD
+//);
+
+LPVOID
+WINAPI
+hkVirtualAllocEx
+(
+  _In_ HANDLE hProcess,
+  _In_opt_ LPVOID lpAddress,
+  _In_ SIZE_T dwSize,
+  _In_ DWORD flAllocationType,
+  _In_ DWORD flProtect
+);
+
+
+
+//extern definitions
+////////////////////
+// 
+//extern tWriteProcessMemory oWriteProcessMemory;
 extern tVirtualAllocEx oVirtualAllocEx;
 
-
-#define ANSI_COLOR_RED     "\x1b[31m"
-#define ANSI_COLOR_GREEN   "\x1b[32m"
-#define ANSI_COLOR_YELLOW  "\x1b[33m"
-#define ANSI_COLOR_BLUE    "\x1b[34m"
-#define ANSI_COLOR_MAGENTA "\x1b[35m"
-#define ANSI_COLOR_CYAN    "\x1b[36m"
-#define ANSI_COLOR_RESET   "\x1b[0m"
+extern tNtWriteVirtualMemory oNtWriteVirtualMemory;
